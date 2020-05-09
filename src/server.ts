48e37253a6cdbc8d5028f493e90b93659fbb0530
Aleksandr Kanunnikov <lifeart92@gmail.com>
2020-05-09 20:39:49 +0300

    fix component generation api

--- src/server.ts
+++ src/server.ts
@@ -128,9 +128,9 @@ export default class Server {
       };
       this.connection.workspace.applyEdit(edit);
       try {
-        await this.executors['els.executeInEmberCLI'](this, '', [`ember g component ${componentName}`]);
-        await new Promise((resolve) => setTimeout(resolve, 1000));
+        await this.executors['els.executeInEmberCLI'](this, '', [`g component ${componentName}`]);
         const registry = this.getRegistry(filePath);
+        logInfo(JSON.stringify(registry));
         const fileName = registry['components'][componentName].find((file: string) => file.includes('/components/') && !file.includes('/test'));
         const edit: WorkspaceEdit = {
           changes: {
