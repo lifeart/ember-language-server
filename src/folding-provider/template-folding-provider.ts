ed7672bfa928b865f8625a04c986bc3a01fbc5ac
Aleksandr Kanunnikov <lifeart92@gmail.com>
2022-02-11 22:07:48 +0300

    update snapshots

--- src/folding-provider/template-folding-provider.ts
+++ src/folding-provider/template-folding-provider.ts
@@ -12,23 +12,23 @@ export default class TemplateFoldingProvider {
       BlockStatement(node: ASTv1.BlockStatement) {
         const loc = node.loc.toJSON();
 
-        results.push(FoldingRange.create(loc.start.line, loc.end.line, loc.start.column, loc.end.column, FoldingRangeKind.Region));
+        results.push(FoldingRange.create(loc.start.line - 1, loc.end.line - 1, loc.start.column, loc.end.column, FoldingRangeKind.Region));
 
         if (node.inverse) {
           const loc = node.inverse.loc.toJSON();
 
-          results.push(FoldingRange.create(loc.start.line, loc.end.line, loc.start.column, loc.end.column, FoldingRangeKind.Region));
+          results.push(FoldingRange.create(loc.start.line - 1, loc.end.line - 1, loc.start.column, loc.end.column, FoldingRangeKind.Region));
         }
       },
       MustacheCommentStatement(node: ASTv1.MustacheCommentStatement) {
         const loc = node.loc.toJSON();
 
-        results.push(FoldingRange.create(loc.start.line, loc.end.line, loc.start.column, loc.end.column, FoldingRangeKind.Comment));
+        results.push(FoldingRange.create(loc.start.line - 1, loc.end.line - 1, loc.start.column, loc.end.column, FoldingRangeKind.Comment));
       },
       ElementNode(node: ASTv1.ElementNode) {
         const loc = node.loc.toJSON();
 
-        results.push(FoldingRange.create(loc.start.line, loc.end.line, loc.start.column, loc.end.column, FoldingRangeKind.Region));
+        results.push(FoldingRange.create(loc.start.line - 1, loc.end.line - 1, loc.start.column, loc.end.column, FoldingRangeKind.Region));
       },
     });
 
